9c842ec5b9dcd1c8aea961a9f8872989cb9d3735
David Higgins <zoul0813@me.com>
2023-10-16 09:39:55 -0400

    * added hello world program from applefritter, commented out for now (need to disassemble eventually)

--- rom/rom.s
+++ rom/rom.s
@@ -18,8 +18,15 @@ loop:
 	LDA KBD         ; Load character. B7 should be ‘1’.
 	STA DSP
 	STA $01
+	CLC
 :
-	JMP loop
+	BCC :-
+
+
+;;; Hello World from https://www.applefritter.com/comment/60111#comment-60111
+  ; 280    00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F
+	; .byte $A2, $0C, $BD, $8B, $02, $20, $EF, $FF, $CA, $D0, $F7, $60, $8D, $C4, $CC, $D2
+	; .byte $CF, $D7, $A0, $CF, $CC, $CC, $C5, $C8
 
 UAPUTW:
 	RTS
